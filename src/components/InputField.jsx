2dd1858ea78d16dda2c13b8791562264d0865ce3
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-01-27 15:35:53 -0600

    format with prettier

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -10,14 +10,20 @@ const InputField = () => {
   //maybe can use report validity to filter certain items?
   const HandleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData,' this is formdata')
+    console.log(formData, " this is formdata");
     // Search(e.target.value)
   };
   return (
     <div>
       <form onSubmit={HandleSubmit}>
-      <label htmlFor="title">Title</label>
-      <input onChange={(e) => setFormData({...formData, title: e.target.value})} value={formData.title} type="text" name="title" id="title" />
+        <label htmlFor="title">Title</label>
+        <input
+          onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+          value={formData.title}
+          type="text"
+          name="title"
+          id="title"
+        />
       </form>
       <RecipeFilters />
     </div>
