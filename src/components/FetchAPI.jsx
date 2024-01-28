1459f617840e2b83a3f8e7901a1d55d6ed447c07
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-01-28 10:59:22 -0600

    fetchapi works

--- src/components/FetchAPI.jsx
+++ src/components/FetchAPI.jsx
@@ -5,39 +5,40 @@ const APP_ID = "07219864";
 const APP_key = "f83320eb349bdebfd72d4be76b29e58f";
 
 const useFetchAPI = ({ formData }) => {
-
-  const { searchQuery } = formData
-  console.log(searchQuery, 'this is searchquery')
+  const { searchQuery } = formData;
+  console.log(searchQuery, "this is searchquery");
   const [items, setItems] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getData = async () => {
-      try {
-        const response = await fetch(
-          `https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}app_id=${APP_ID}&app_key=${APP_key}`,
-          {
-            mode: "cors",
-          }
-        );
-        if (!response.ok) {
-          throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
+      if (formData) {
+        try {
+          const response = await fetch(
+            `https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}`,
+            {
+              mode: "cors",
+            }
           );
+          if (!response.ok) {
+            throw new Error(
+              `This is an HTTP error: The status is ${response.status}`
+            );
+          }
+          let data = await response.json();
+          setItems(data);
+          setError(null);
+        } catch (err) {
+          setError(err.message);
+          setItems(null);
+        } finally {
+          setLoading(false);
         }
-        let data = await response.json();
-        setItems(data);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        setItems(null);
-      } finally {
-        setLoading(false);
       }
     };
     getData();
-  }, [searchQuery]);
+  }, [searchQuery, formData]);
 
   return { items, error, loading };
 };
--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
 import useFetchAPI from "./FetchAPI";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import RecipeFilters from "./RecipeFilters";
 
 //need to somehow take the value from this search, and hand it to the fetchapi
 
-//maybe pass this state up? 
+//maybe pass this state up?
 
 //remove state, pass it to parent
 
 const InputField = ({ formData, setFormData }) => {
-  const { items } = useFetchAPI();
-  console.log(items, 'this is items')
+  const { items, error, loading } = useFetchAPI({ formData });
+  console.log(items, "this is items");
 
   //maybe can use report validity to filter certain items?
   const HandleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData.title, 'this is formdata')
-    if (formData.title === 'star fruit') {
-      console.log('error this is starfruit')
-      return
+    console.log(formData.title, "this is formdata");
+    if (formData.title === "star fruit") {
+      console.log("error this is starfruit");
+      return;
     }
     // Search(e.target.value)
   };
@@ -28,7 +28,9 @@ const InputField = ({ formData, setFormData }) => {
       <form onSubmit={HandleSubmit}>
         <label htmlFor="title">Title</label>
         <input
-          onChange={(e) => setFormData((prevData) => ({ ...prevData, title: e.target.value }))}
+          onChange={(e) =>
+            setFormData((prevData) => ({ ...prevData, title: e.target.value }))
+          }
           value={formData.title}
           type="text"
           name="title"
@@ -43,6 +45,6 @@ const InputField = ({ formData, setFormData }) => {
 InputField.propTypes = {
   formData: PropTypes.object,
   setFormData: PropTypes.func,
-}
+};
 
 export default InputField;
