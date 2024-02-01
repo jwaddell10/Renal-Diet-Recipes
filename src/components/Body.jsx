04d3ebff5aa2206e8025f18139ca2a9d5b59ff36
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-02-01 15:50:46 -0600

    remove style type from listitems in body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,4 +1,8 @@
 import PropTypes from "prop-types";
+import styled from "styled-components";
+import styles from "./Body.module.css";
+
+
 function Body({ itemsFromInputField }) {
   let recipes = null;
   let recipeIngredients = null;
@@ -16,27 +20,45 @@ function Body({ itemsFromInputField }) {
 
   return (
     itemsFromInputField && (
-      <ul>
-        {recipes &&
-          recipes.map((item) => (
-            <li key={item.recipe.uri}>
-              <ul>
-                {item.recipe.label}
-                {Math.floor(item.recipe.calories)}
-              </ul>
-              {item.recipe.ingredientLines.map((ingredient, index) => (
-                <li key={index}>{ingredient}</li>
-              ))}
-              <img src={item.recipe.image} />
-              <button onClick={() => urlRedirect(item.recipe.url)}>
-                Recipe
-              </button>
+       <ul><StyledSection>
+      {recipes &&
+        recipes.map((item) => (
+          <StyledCard key={item.recipe.uri}>
+            <li className={styles.li}>
+              <div>
+                <div>
+                  <ul>{item.recipe.label}</ul>
+                  <ul>Calories: {Math.floor(item.recipe.calories)}</ul>
+                  {item.recipe.ingredientLines.map((ingredient, index) => (
+                    <li className={styles.li} key={index}>{ingredient}</li>
+                  ))}
+                </div>
+                <img src={item.recipe.image} />
+                <button onClick={() => urlRedirect(item.recipe.url)}>
+                  Recipe
+                </button>
+              </div>
             </li>
-          ))}
-      </ul>
+          </StyledCard>
+        ))}</StyledSection>
+    </ul>
+     
     )
   );
 }
+
+const StyledSection = styled.section`
+  display: grid;
+  grid-gap: 3rem;
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  background-color: #fff;
+  color: #444;
+`;
+
+const StyledCard = styled.section`
+display: flex;
+border: 1px solid black;
+`
 
 Body.propTypes = {
   itemsFromInputField: PropTypes.object,
